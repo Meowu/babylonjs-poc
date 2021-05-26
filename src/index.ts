6d9c7b57b882248eaa1f400dfea0e8afccc0b317
Meowu <474384902@qq.com>
2021-05-26 15:19:42 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera"
+import { Engine } from "@babylonjs/core/Engines/engine"
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight"
+import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder"
+import { Scene } from "@babylonjs/core/scene"
+import { Vector3 } from "@babylonjs/core/Maths/math.vector"
+
+import { SampleMaterial } from "./Materials/SampleMaterial"
+
+const view = document.getElementById("view") as HTMLCanvasElement
+const engine = new Engine(view, true)
+
+const scene = new Scene(engine)
+
+const camera = new ArcRotateCamera(
+    "camera",
+    Math.PI / 2,
+    Math.PI / 3.2,
+    2,
+    Vector3.Zero(),
+    scene)
+
+camera.attachControl(view)
+
+const light = new HemisphericLight(
+    "light",
+    new Vector3(0, 1, 0),
+    scene)
+
+const mesh = MeshBuilder.CreateGround("mesh", {}, scene)
+
+const material =  new SampleMaterial("material", scene)
+mesh.material = material
+
+engine.runRenderLoop(() => {
+    scene.render();
+})
